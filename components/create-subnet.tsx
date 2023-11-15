f93487c61e7f598333c31784bcc77f2f328d9d1c
Mikers <snissn@gmail.com>
2023-11-14 16:53:53 -1000

    update for step4

--- components/create-subnet.tsx
+++ components/create-subnet.tsx
@@ -3,44 +3,58 @@ import { useState } from 'react';
 
 const GenerateValidators = ({ title, text }) => {
   const [validatorCount, setValidatorCount] = useState(1);
-  const [minValidatorStake] = useState(1); // Fixed value for Minimum Validator Stake
-  const [bottomUpCheckPeriod, setBottomUpCheckPeriod] = useState(30); // Default value for Bottom Up Check Period
+  const [minValidatorStake] = useState(1);
+  const [bottomUpCheckPeriod, setBottomUpCheckPeriod] = useState(30);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    // Add logic for key generation, address computation, and faucet interaction
+    // Logic for handling form submission
   };
 
   return (
     <div className="container mx-auto p-4 max-w-7xl sm:px-6 lg:px-8">
       <form
         onSubmit={handleSubmit}
-        className="space-y-6 bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+        className="grid grid-cols-2 gap-4 bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+        aria-labelledby="validator-form"
       >
-        <div>
-          <label>Number of Validators: {validatorCount}</label>
-          <input
-            type="range"
-            min="1"
-            max="1" // Assuming max range for validators, adjust as necessary
-            value={validatorCount}
-            onChange={(e) => setValidatorCount(e.target.value)}
-          />
+        <div className="flex items-center">
+          <span className="icon-validator-count mr-2" />{' '}
+          {/* Icon for visual aid */}
+          <label htmlFor="validatorCount">
+            Number of Validators: {validatorCount}
+          </label>
         </div>
-        <div>
+        <input
+          id="validatorCount"
+          type="range"
+          min="1"
+          max="100"
+          value={validatorCount}
+          onChange={(e) => setValidatorCount(e.target.value)}
+          className="range-slider"
+        />
+        <div className="flex items-center">
+          <span className="icon-min-stake mr-2" /> {/* Icon for visual aid */}
           <label>Minimum Validator Stake: {minValidatorStake}</label>
         </div>
-        <div>
-          <label>Bottom Up Check Period (blocks): </label>
-          <input
-            type="number"
-            value={bottomUpCheckPeriod}
-            onChange={(e) => setBottomUpCheckPeriod(e.target.value)}
-          />
+        <div> {/* Non-editable field for minimum stake */}</div>
+        <div className="flex items-center">
+          <span className="icon-bottom-up mr-2" /> {/* Icon for visual aid */}
+          <label htmlFor="bottomUpCheckPeriod">
+            Bottom Up Check Period (blocks):{' '}
+          </label>
         </div>
+        <input
+          id="bottomUpCheckPeriod"
+          type="number"
+          value={bottomUpCheckPeriod}
+          onChange={(e) => setBottomUpCheckPeriod(e.target.value)}
+          className="number-input"
+        />
         <button
           type="submit"
-          className="mt-4 bg-blue-500 text-white p-2 rounded"
+          className="  bg-blue-500 hover:bg-blue-700 text-white p-2 rounded button"
         >
           Create
         </button>
@@ -60,7 +74,7 @@ const GenerateValidators = ({ title, text }) => {
       </div>
       <p className="p-8">
         <Link
-          href="/step4"
+          href="/step5"
           className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
         >
           Next &gt;
